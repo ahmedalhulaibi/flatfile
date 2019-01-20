981d616367b639a46ac65966083c5e8308552223
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-01-19 23:01:08 -0500

    docs: Add RADME and example

---
+++ README.md
@@ -0,0 +1,14 @@
+# ffparser
+
+This library provides a method `Unmarshal` which maps a slice of bytes to a struct based on defined struct tags.
+
+Struct tags are in the form `ffp:"pos,len"`
+
+Example: 
+```go
+type FileHeader struct {
+	LogicalRecordTypeID byte   `ffp:"1,1"`
+	LogicalRecordCount  uint32 `ffp:"2,9"`
+	OriginatorID        string `ffp:"11,10"`
+}
+```
---
+++ example/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ahmedalhulaibi/ffparser"
+)
+
+type FileHeader struct {
+	LogicalRecordTypeID byte   `ffp:"1,1"`
+	LogicalRecordCount  uint32 `ffp:"2,9"`
+	NameIdentifier      string `ffp:"11,10"`
+}
+
+func main() {
+	file, err := os.Open("./test.txt")
+	checkError(err)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	eof := false
+	for !eof {
+		data := readLine(reader)
+		if data == nil {
+			eof = true
+		} else {
+			fileHeader := &FileHeader{}
+			err := ffparser.Unmarshal(data, fileHeader)
+			fmt.Printf("%#v\n", fileHeader)
+			checkError(err)
+		}
+	}
+	checkError(err)
+}
+
+func readLine(reader *bufio.Reader) []byte {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return nil
+	}
+
+	return str
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
