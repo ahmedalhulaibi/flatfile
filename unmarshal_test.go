4971c1dbef574bc550fa3512c39953a59e184429
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-03-16 02:14:31 -0400

    refactor: Rename ffparser_test.go to unmarshal_test.go
    test: Removed parseFfpTag tests from unmarshal_test.go

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -646,17 +646,6 @@ func TestFloat64OutOfRangeErr_Unmarshal(t *testing.T) {
 	t.Log(err)
 }
 
-func TestFfpTagParsePosSyntaxErr_parseFfpTag(t *testing.T) {
-	testVal := `ffp:"asdf,1"`
-
-	err := parseFfpTag(testVal, &ffpTagType{})
-
-	if err == nil {
-		t.Error("parseFfpTag should return syntax error when failing to parse position param")
-	}
-	t.Log(testVal)
-	t.Log(err)
-}
 func TestFfpTagParsePosSyntaxErr_Unmarshal(t *testing.T) {
 	type FfpTest struct {
 		TestVal string `ffp:"asdf,1"`
@@ -669,18 +658,6 @@ func TestFfpTagParsePosSyntaxErr_Unmarshal(t *testing.T) {
 
 	if err == nil {
 		t.Error("Unmarshal should return syntax error when failing to parse position param")
-	}
-	t.Log(testVal)
-	t.Log(err)
-}
-
-func TestFfpTagParseLenSyntaxErr_parseFfpTag(t *testing.T) {
-	testVal := `ffp:"1,asdf"`
-
-	err := parseFfpTag(testVal, &ffpTagType{})
-
-	if err == nil {
-		t.Error("parseFfpTag should return syntax error when failing to parse length param")
 	}
 	t.Log(testVal)
 	t.Log(err)
@@ -703,17 +680,6 @@ func TestFfpTagParseLenSyntaxErr_Unmarshal(t *testing.T) {
 	t.Log(err)
 }
 
-func TestFfpTagParsePosRangeErr_parseFfpTag(t *testing.T) {
-	testVal := `ffp:"-1,10"`
-
-	err := parseFfpTag(testVal, &ffpTagType{})
-
-	if err == nil {
-		t.Error("parseFfpTag should return out of range error when failing to parse position param")
-	}
-	t.Log(testVal)
-	t.Log(err)
-}
 func TestFfpTagParsePosRangeErr_Unmarshal(t *testing.T) {
 	type FfpTest struct {
 		TestVal string `ffp:"-1,10"`
@@ -731,18 +697,6 @@ func TestFfpTagParsePosRangeErr_Unmarshal(t *testing.T) {
 	t.Log(err)
 }
 
-func TestFfpTagParseLenRangeErr_parseFfpTag(t *testing.T) {
-	testVal := `ffp:"1,-1"`
-
-	err := parseFfpTag(testVal, &ffpTagType{})
-
-	if err == nil {
-		t.Error("parseFfpTag should return out of range error when failing to parse length param")
-	}
-	t.Log(testVal)
-	t.Log(err)
-}
-
 func TestFfpTagParseLenRangeErr_Unmarshal(t *testing.T) {
 	type FfpTest struct {
 		TestVal string `ffp:"1,-1"`
@@ -755,18 +709,6 @@ func TestFfpTagParseLenRangeErr_Unmarshal(t *testing.T) {
 
 	if err == nil {
 		t.Error("Unmarshal should return out of range error when failing to parse length param")
-	}
-	t.Log(testVal)
-	t.Log(err)
-}
-
-func TestFfpTagParseMissingParamErr_parseFfpTag(t *testing.T) {
-	testVal := `ffp:""`
-
-	err := parseFfpTag(testVal, &ffpTagType{})
-
-	if err == nil {
-		t.Error("parseFfpTag should return missing parameter error")
 	}
 	t.Log(testVal)
 	t.Log(err)
