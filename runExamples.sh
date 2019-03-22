3a9ea659804f0921f7ebb70fe7d9591da41c2f75
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-03-21 22:10:58 -0400

    chore: Add script to run all examples.
    This is useful for making sure all the example code actually runs

---
+++ runExamples.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+check_err () {
+    err=$?
+    if [ $err -eq 1 ]; then
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        echo "Error occurred status: $err"
+        exit
+    fi
+}
+#get root directory of git repo
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd $GIT_ROOT/example
+echo $GIT_ROOT
+for example_dir in *; do
+    cd $GIT_ROOT/example/$example_dir
+    case "$example_dir" in
+        "bufferedReadFile")
+            echo "Running example/$example_dir option 1"
+            go run main.go 1
+            check_err
+            echo "*************************************************"
+            echo "Running example/$example_dir option 2"
+            go run main.go 2
+            check_err
+            ;;
+        *) 
+            echo "Running example/$example_dir"
+            go run main.go
+            check_err
+            ;;
+    esac
+    echo "================================================="
+done
